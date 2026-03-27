480e2cf74c679b424c93dc0d3687819c892b7a7d
Evan Wall <elwwcs@gmail.com>
2026-03-26 22:19:47 -0300

    Added Student Class to Project

---
+++ oopFinalProjecy/oopFinalProjecy/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopFinalProjecy
+{
+    internal class Student : User
+    {
+        int BorrowLimit = 5; // Will default to 5
+
+        int BooksBorrowed = 0; // Will increase as books are borrowed
+
+        public Student()
+        {
+            // Default, does nothing
+        }
+
+        public Student(int br)
+        {
+            // Overloaded, will Call base
+        }
+
+    }
+}
