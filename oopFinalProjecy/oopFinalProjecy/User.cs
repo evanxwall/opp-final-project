26b19ed61a534200f1d848580dcad764f29d822f
Evan Wall <elwwcs@gmail.com>
2026-03-26 22:15:33 -0300

    Added Draft of Abstract User Class to Project

---
+++ oopFinalProjecy/oopFinalProjecy/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopFinalProjecy
+{
+    internal abstract class User
+    {
+        string Name { get; set; }
+
+        int UserId { get; set; }
+
+        string Role { get; set; }
+
+        // BookList MyBooks will need to be added here
+
+        public User()
+        {
+            // Default Constructor, needs to Intialize Name and MyBooks
+        }
+
+        public User(string name, int userId)
+        {
+            // Sends Name and Role only, calls the default
+        }
+
+
+
+    }
+}
