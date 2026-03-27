1bd6982c37e3bebdb2cbdb15a18e70cd204273ad
Evan Wall <elwwcs@gmail.com>
2026-03-26 22:28:54 -0300

    Added IBorrowable Interface to the project

---
+++ oopFinalProjecy/oopFinalProjecy/IBorrowable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopFinalProjecy
+{
+    internal interface IBorrowable
+    {
+        void Borrow(User user); // Doesnt Return Anything
+        void Return(User user); // Doesnt Return Anything
+    }
+}
