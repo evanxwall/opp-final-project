b7908d6e612c19964c96bbe5ad7cbce93f9939ad
Evan Wall <elwwcs@gmail.com>
2026-03-26 22:21:05 -0300

    Added Teacher Class to Project

---
+++ oopFinalProjecy/oopFinalProjecy/Teacher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopFinalProjecy
+{
+    internal class Teacher : User
+    {
+        int BorrowLimit = 10; // Will default to 10
+
+        int BooksBorrowed = 0; // Will increase as books are borrowed
+
+        public Teacher()
+        {
+            // Default, does nothing
+        }
+
+        public Teacher(int br)
+        {
+            // Overloaded, will Call base
+        }
+
+
+
+    }
+}
