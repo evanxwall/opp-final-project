0967e4e94d01187483b456cc2daf3f4243171939
Evan Wall <elwwcs@gmail.com>
2026-03-26 22:23:07 -0300

    Added Sealed Librarian Class to the Project

---
+++ oopFinalProjecy/oopFinalProjecy/Librarian.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopFinalProjecy
+{
+    internal sealed class Librarian : User
+    {
+        int BorrowLimit; // No Borrow Limit
+
+        public Librarian()
+        {
+            // Default, does nothing
+        }
+
+        public Librarian(int BorrowLimit)
+        {
+            // Overloaded, will just call base.
+        }
+
+
+
+
+
+
+    }
+}
