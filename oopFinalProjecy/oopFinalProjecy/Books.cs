ec523e436acbb7e43504cb0b73c357d7de82c370
Evan Wall <elwwcs@gmail.com>
2026-03-29 15:22:11 -0300

    Cleaned up Books.cs / Almost done, missing Object.Equals() and Object.GetHashCode() based on Visual Studio Suggestions

--- oopFinalProjecy/oopFinalProjecy/Books.cs
+++ oopFinalProjecy/oopFinalProjecy/Books.cs
@@ -6,37 +6,42 @@ using System.Threading.Tasks;
 
 namespace oopFinalProjecy
 {
-    internal class Books
+    internal class Books : IBorrowable
     {
         string Title { get; set; }
-
         string Author { get; set; }
-        bool IsAvailable { get; set; } // Will default to true
+        bool IsAvailable { get; set; } = true;
 
         public Books()
         {
-            // Default Constructor
+            // Default
         }
 
-        public Books(string title, string author, bool isAvailable)
+        public Books(string title, string author)
         {
             // Overloaded
             Title = title;
             Author = author;
-            IsAvailable = isAvailable;
         }
 
-
-        // These Aren't Completely Right but just wanted a template down
-        public static Books operator ==(Books book1,  Books book2)
+        public static bool operator ==(Books book1,  Books book2)
         {
-            return book1 == book2;
+            if (ReferenceEquals(book1, book2)) 
+                return true;
+
+            if (book1 is null || book2 is null) 
+                return false;
+
+            return book1.Title == book2.Title && book1.Author == book2.Author;
         }
 
-        public static Books operator !=(Books book1, Books book2)
+        public static bool operator !=(Books book1, Books book2)
         {
-            return book1 != book2;
+            return !(book1 == book2);
         }
+
+        public void Borrow(User user) => IsAvailable = false;
+        public void Return(User user) => IsAvailable = true;
 
     }
 }
