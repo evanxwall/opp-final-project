f54ed4908919d83dc216c9c4f373c7d9cb26cdc5
Evan Wall <elwwcs@gmail.com>
2026-04-07 14:14:38 -0300

    Intialized MSTest Project for Books classes to test methods

---
+++ oopFinalProjecy/TestBooksMethods/BooksTests.cs
@@ -0,0 +1,33 @@
+﻿namespace TestBooksMethods
+{
+    [TestClass]
+    public sealed class BooksTests
+    {
+        [TestMethod]
+        public void TestBorrowMethod()
+        {
+            // Test 1: Borrow() Method
+
+
+
+        }
+
+        [TestMethod]
+        public void TestReturnMethod()
+        {
+            // Test 2: Return() Method
+
+
+
+        }
+
+        [TestMethod]
+        public void TestOverloadedOperators()
+        {
+            // Test 3: == and != overloaded operators
+
+
+
+        }
+    }
+}
